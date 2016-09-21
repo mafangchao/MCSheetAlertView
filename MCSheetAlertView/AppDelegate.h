07edf71f9b00c7f2d259c616f6c7ee9578454aae
小马哥 <15618933468@163.com>
2016-09-21 15:08:55 +0800

    Initial Commit

---
+++ MCSheetAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCSheetAlertView
+//
+//  Created by 123 on 16/9/21.
+//  Copyright © 2016年 machao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCSheetAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCSheetAlertView
+//
+//  Created by 123 on 16/9/21.
+//  Copyright © 2016年 machao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
